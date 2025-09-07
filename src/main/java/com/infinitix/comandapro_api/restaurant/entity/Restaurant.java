54395c8fd712e0636835540873d18ae4f926ed8f
FelipeK57 <kevinboor5725@gmail.com>
2025-09-07 12:16:21 -0500

    Update .gitignore and refactor Restaurant entity; externalize database credentials

--- src/main/java/com/infinitix/comandapro_api/restaurant/entity/Restaurant.java
+++ src/main/java/com/infinitix/comandapro_api/restaurant/entity/Restaurant.java
@@ -6,8 +6,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "restaurants")
 public class Restaurant {
 
@@ -18,4 +26,6 @@ public class Restaurant {
     @Column(unique = true, nullable = false)
     private String name;
 
+    @Column(nullable = false)
+    private boolean active;
 }
